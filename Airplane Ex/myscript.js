f21014ba2cb2784c0dfa4a447073cb77cdd16fdb
David Riediger <x128@gmx.at>
2019-10-09 14:18:28 +0200

    Awesome Airplane

--- Airplane Ex/myscript.js
+++ Airplane Ex/myscript.js
@@ -2,19 +2,29 @@
 
 $("document").ready(function(){
 
+/*	$("#moveme").contextmenu(function(){
+		return false;
+	})*/
+
+	$("#moveme").on("mouseover", function(){
+		$("#moveme img").attr("src", "airplane.png");
+	})
+
 	$("#moveme").on("mousemove", function(e){
 		x = e.pageX;
 		y = e.pageY;
 		$("#moveme").css({
+			// "position": "fixed",
 			"top": y-128,
 			"left": x-128
 		})
 		$("#demo").text("x:" + x + "/y:" + y);
 	});
 
-	$("#moveme").click(function(){
+	$("#moveme").click(function(e){
+		console.log(e.which);
 		$(this).animate({
-			top: windowHeight-256,
+			top: windowHeight-200, //could have used bottom
 			opacity: 0
 		},1000, crash).animate({
 			// deg: 180,
@@ -22,11 +32,21 @@ $("document").ready(function(){
 		},1000);
 	});
 
+	$("#moveme").contextmenu(function(e){ //RIGHT CLICK EVENT (e.which==2 didn't work)
+		document.getElementById('boost').play();
+		$("#moveme img").attr("src","airplane-boost.png");
+		setTimeout(function(){
+			$("#moveme img").attr("src","airplane.png")
+		},2500)
+		return false; //disables the context menu
+	});
+
 });
 // End of JQuery Code
 
 function crash(){
 	document.getElementById('crash').play();
+	$("#moveme img").attr("src","airplane-crashed.png");
 }
 
 var windowHeight = window.innerHeight;
